b117b331a1c67f1e015a54ec444edb4b5c4cfcba
busenurdmb <bus.dmb@gmail.com>
2025-05-07 21:13:35 +0300

    +Güncellem ve silme category kısmı için

--- AcunMedyaAkademiWebUI/AcunMedyaAkademiWebUI/Controllers/CategoryController.cs
+++ AcunMedyaAkademiWebUI/AcunMedyaAkademiWebUI/Controllers/CategoryController.cs
@@ -53,9 +53,9 @@ namespace AcunMedyaAkademiWebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateCategory(CreateCategoryDto model)
         {
-            var client= _httpClientFactory.CreateClient();
-            var jsondata=JsonConvert.SerializeObject(model);
-            var content=new StringContent(jsondata,Encoding.UTF8,"application/json");
+            var client = _httpClientFactory.CreateClient();
+            var jsondata = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsondata, Encoding.UTF8, "application/json");
 
 
             var response = await client.PostAsync("https://localhost:7276/api/Categories", content);
@@ -67,7 +67,44 @@ namespace AcunMedyaAkademiWebUI.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7276/api/Categories/" + id);
+            var jsondata = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsondata);
+            return View(values);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(int id,UpdateCategoryDto model)
+        {
+            var client= _httpClientFactory.CreateClient();
+            var jsondata=JsonConvert.SerializeObject(model);
+            var content=new StringContent(jsondata, Encoding.UTF8, "application/json");
+
+            var response = await client.PutAsync($"https://localhost:7276/api/Categories/{id}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+
+            var response = await client.DeleteAsync("https://localhost:7276/api/Categories/" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
 
     }
 }
